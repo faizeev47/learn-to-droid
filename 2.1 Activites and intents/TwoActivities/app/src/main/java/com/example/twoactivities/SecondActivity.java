8d57f5c4447e9ab65845366f95588f52cae27618
Muhammad Faizan Badar <faizan.badar97@outlook.com>
2019-10-22 00:47:26 +0500

    Return data back to the main activity

--- 2.1 Activites and intents/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ 2.1 Activites and intents/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+
+    private EditText mReplyEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,14 @@ public class SecondActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
+        mReplyEditText = findViewById(R.id.editText_second);
+    }
+
+    public void returnReply(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        String message = mReplyEditText.getText().toString();
+        intent.putExtra(EXTRA_REPLY, message);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
