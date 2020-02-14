dcf4ad1356343f9d109f99d48426a115ac8ab66e
Muhammad Faizan Badar <faizan.badar97@outlook.com>
2020-02-14 06:23:11 +0500

    Added items to the options menu

---
+++ 4.3 Menus and pickers/DroidCafeInput/app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.droidcafe;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.android.droidcafe.mainactivity.extra.MESSAGE";
+
+    private String mOrderMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mOrderMessage = "";
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mOrderMessage.length() > 0) {
+                    Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                    intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                    startActivity(intent);
+                }
+                else {
+                    displayToast("Order something first, will ya'?");
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_order) {
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Shows a message that the donut image was clicked
+     * @param view
+     */
+    public void showDonutOrder(View view) {
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    /**
+     * Shows a message that the ice cream image was clicked
+     * @param view
+     */
+    public void showIcecreamOrder(View view) {
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    /**
+     * Shows a message that the froyo image was clicked
+     * @param view
+     */
+    public void showFroyoOrder(View view) {
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ 4.3 Menus and pickers/DroidCafeInput/app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -0,0 +1,109 @@
+package com.example.droidcafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class OrderActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
+
+    private EditText oPickUpEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        TextView textViewOrder = findViewById(R.id.textView_order);
+        textViewOrder.setText("Order: " + getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE));
+
+        oPickUpEditText = findViewById(R.id.editText_pickup);
+        if (oPickUpEditText != null) {
+            oPickUpEditText.setVisibility(EditText.INVISIBLE);
+            oPickUpEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    boolean handled = false;
+                    if (actionId == EditorInfo.IME_ACTION_SEND) {
+                        dialNumber(v.getText().toString());
+                        handled = true;
+                    }
+                    return handled;
+                }
+            });
+        }
+
+        findViewById(R.id.nextday).performClick();
+
+        Spinner spinner = findViewById(R.id.spinner_phoneType);
+        if (spinner != null) {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.labels_array,
+                    android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+            spinner.setOnItemSelectedListener(this);
+        }
+
+
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        if (checked) {
+            switch (view.getId()) {
+                case R.id.sameday:
+                    displayToast(getString(R.string.same_day_messenger_service));
+                    oPickUpEditText.setVisibility(EditText.INVISIBLE);
+                    break;
+                case R.id.nextday:
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                    oPickUpEditText.setVisibility(EditText.INVISIBLE);
+                    break;
+                case R.id.pickup:
+                    displayToast(getString(R.string.pick_up));
+                    oPickUpEditText.setVisibility(EditText.VISIBLE);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void dialNumber(String number) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + number));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        else {
+            Log.e("Implicit intents", "Cannot open the dialer!");
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
