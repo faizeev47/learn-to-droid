9dffa8d2ba87357a7dd28499fffd3bcd149b6bdf
Muhammad Faizan Badar <faizan.badar97@outlook.com>
2020-02-15 02:00:19 +0500

    Completed coding challenge

---
+++ 4.4 User navigation/DrawerExperiment/app/src/main/java/com/example/drawerexperiment/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.drawerexperiment;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_settings)
+                .setDrawerLayout(drawer)
+                .build();
+    }
+
+    public void toastItem(MenuItem item) {
+        Toast.makeText(getApplicationContext(), item.getTitle().toString() + " pressed!", Toast.LENGTH_LONG).show();
+    }
+}
