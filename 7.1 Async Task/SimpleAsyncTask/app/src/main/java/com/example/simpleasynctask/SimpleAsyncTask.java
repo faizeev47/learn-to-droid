7fcb213d8666a047dc6deab4d526e1e55678657a
Muhammad Faizan Badar <faizan.badar97@outlook.com>
2020-02-16 05:17:24 +0500

    Completed coding challenge

--- 7.1 Async Task/SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
+++ 7.1 Async Task/SimpleAsyncTask/app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
@@ -1,12 +1,18 @@
 package com.example.simpleasynctask;
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
+import java.sql.Time;
+import java.time.Clock;
 import java.util.Random;
 
-public class SimpleAsyncTask extends AsyncTask <Void, Void, String> {
+import static java.lang.System.currentTimeMillis;
+
+public class SimpleAsyncTask extends AsyncTask <Void, Long, String> {
     private WeakReference<TextView> mTextView;
 
     public SimpleAsyncTask(TextView tv) {
@@ -15,8 +21,9 @@ public class SimpleAsyncTask extends AsyncTask <Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... voids) {
-        int sleepTime = new Random().nextInt(11) * 20;
+        int sleepTime = (new Random().nextInt(30) + 20) * 20;
         try {
+            new TimerThread(currentTimeMillis(), sleepTime).start();
             Thread.sleep(sleepTime);
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -28,4 +35,31 @@ public class SimpleAsyncTask extends AsyncTask <Void, Void, String> {
     public void onPostExecute(String result) {
         mTextView.get().setText(result);
     }
+
+    @Override
+    protected void onProgressUpdate(Long... values) {
+        super.onProgressUpdate(values);
+        String message = "I have been sleeping for " + values[0] + " milliseconds.";
+        mTextView.get().setText(message);
+    }
+
+
+    class TimerThread extends Thread {
+
+        long startTime;
+        int sleepTime;
+        public TimerThread(long startTime, int sleepTime) {
+            this.startTime = startTime;
+            this.sleepTime = sleepTime;
+        }
+
+        public void run() {
+            long timeSlept = currentTimeMillis() - startTime;
+            while (timeSlept < sleepTime) {
+                publishProgress(timeSlept);
+                timeSlept = currentTimeMillis() - startTime;
+            }
+        }
+    }
+
 }
