4c70f39b3f92c0eae00457a9504da34101f86085
Muhammad Faizan Badar <faizan.badar97@outlook.com>
2020-02-20 14:24:57 -0800

    Create an Adapter and adding the RecyclerView

--- 10.1 Part A Room, LiveData, and ViewModel/RoomWordsSample/app/src/main/java/com/example/roomwordssample/MainActivity.java
+++ 10.1 Part A Room, LiveData, and ViewModel/RoomWordsSample/app/src/main/java/com/example/roomwordssample/MainActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.view.Menu;
@@ -20,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
