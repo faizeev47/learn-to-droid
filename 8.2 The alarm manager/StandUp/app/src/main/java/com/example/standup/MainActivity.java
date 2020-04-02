c1d26ad7a0d79781f2275cb3c43d7b9869ab52a0
Muhammad Faizan Badar <faizan.badar97@outlook.com>
2020-04-02 19:35:06 +0500

    Set up the notification

--- 8.2 The alarm manager/StandUp/app/src/main/java/com/example/standup/MainActivity.java
+++ 8.2 The alarm manager/StandUp/app/src/main/java/com/example/standup/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.standup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+
+    private NotificationManager mNotificationManager;
 
     private ToggleButton  mAlarmToggle;
 
@@ -16,14 +29,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createNotifcationChannel();
+
         mAlarmToggle = findViewById(R.id.alarmToggle);
+        mAlarmToggle.setChecked(false);
         mAlarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Toast.makeText(MainActivity.this,
-                        isChecked ? "Stand Up Alarm On!" : "Stand Up Alarm Off!",
-                        Toast.LENGTH_SHORT).show();
+                String toastMessage;
+                if (isChecked) {
+                    toastMessage = "Stand Up Alarm On!";
+                    deliverNotification(MainActivity.this);
+                } else {
+                    toastMessage = "Stand Up Alarm Off!";
+                }
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void createNotifcationChannel() {
+        mNotificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    PRIMARY_CHANNEL_ID,
+                    "Stand Up Notification",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifies every 15 minutes to stand up and walk!");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context) {
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(
+                context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stand_up)
+                .setContentTitle("Stand Up!")
+                .setContentText("You should stand up and walk around now.")
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
